6f553aa1b9d5dcd1da54a93a103f6140b99c420c
Nirajan Malla <nirajannitro5@gmail.com>
2023-05-12 22:31:34 +0545

    created search component

--- src/app/looper/page.js
+++ src/app/looper/page.js
@@ -1,7 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 export default function page() {
+
+    const handleClick = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            I am a looper page!
-        </div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+            <form className="flex flex-col w-full max-w-7xl mx-auto items-center justify-center">
+                <div className="w-full flex flex-col md:flex-row justify-center gap-4 ">
+                    <input className="bg-transparent border rounded-lg px-4 py-2 w-full max-w-xl" placeholder="Enter video link..."></input>
+                    <button onClick={handleClick} className="inline px-4 py-2 rounded-lg border-2 hover:bg-slate-50/10">Search</button>
+                </div>
+            </form>
+        </main>
     )
 }
